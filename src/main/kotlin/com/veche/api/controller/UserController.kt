c4b4246359eda2f7e5ce295d9e56154c3bfc253f
CuriousDanil <curiousdanil@gmail.com>
2025-06-28 22:58:20 +0500

    UserController refined

--- src/main/kotlin/com/veche/api/controller/UserController.kt
+++ src/main/kotlin/com/veche/api/controller/UserController.kt
@@ -1,10 +1,13 @@
 package com.veche.api.controller
 
 import com.veche.api.dto.user.UserSearchRequestDto
+import com.veche.api.dto.user.UserUpdateCredentialsDto
 import com.veche.api.security.UserPrincipal
 import com.veche.api.service.UserService
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,6 +24,12 @@ class UserController(
     @GetMapping("/search")
     fun searchUsersForUserCompany(
         @AuthenticationPrincipal user: UserPrincipal,
-        request: UserSearchRequestDto,
+        @RequestBody request: UserSearchRequestDto,
     ) = userService.searchUsersForUserCompany(request, user)
+
+    @PostMapping
+    fun updateUserCredentials(
+        @AuthenticationPrincipal user: UserPrincipal,
+        @RequestBody request: UserUpdateCredentialsDto,
+    ) = userService.updateUserCredentials(request, user)
 }
